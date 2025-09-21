7b268a02fd283173648d9d6f3bb01fabdd1af74d
EllGHT <rmsgh1188@gmail.com>
2025-09-21 16:54:28 +0900

    refactors some Colors with #colorLiteral

--- Emoji Rangers/Shared/Views/GameColors.swift
+++ Emoji Rangers/Shared/Views/GameColors.swift
@@ -7,8 +7,8 @@ Custom colors for user interface elements.
 import SwiftUI
 
 extension Color {
-    static let gameBackgroundColor = Color(red: 0.988, green: 0.416, blue: 0.239, opacity: 1)
-    static let gameWidgetBackground = Color(red: 0.913, green: 0.737, blue: 0.447, opacity: 1)
+    static let gameBackgroundColor = Color(#colorLiteral(red: 0.988, green: 0.416, blue: 0.239, alpha: 1))
+    static let gameWidgetBackground = Color(#colorLiteral(red: 0.913, green: 0.737, blue: 0.447, alpha: 1))
     static let liveActivityBackground = Color(red: 0.29, green: 0.42, blue: 0.60)
     static let appBackground = Color(red: 0.478, green: 0.808, blue: 1)
     static let textColor = Color.white
